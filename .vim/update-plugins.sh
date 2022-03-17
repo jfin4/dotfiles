cfdbd92b3dea0100b7a239c47f4491bde7ea849e
John Inman <john@johninman.dev>
2022-03-16 19:03:09 -0700

    updates

---
+++ .vim/update-plugins.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+plugins="
+	ervandew/supertab
+	godlygeek/tabular
+	honza/vim-snippets
+	jpalardy/vim-slime
+	preservim/nerdcommenter
+	sirver/ultisnips
+	tpope/vim-repeat
+	tpope/vim-surround
+	"
+
+dir=$HOME/.vim/pack/pack/start
+for p in $plugins 
+do
+    if [ -e $dir/${p##*/} ]
+    then
+        git -C $dir/${p##*/} pull
+    else
+        git clone https://github.com/$p $dir/${p#*/}
+    fi
+done
+
+for d in $dir/*; do
+    if ! { echo "$plugins" | grep -q ${d##*/}; }; then
+        echo move-to-trash $d
+        move-to-trash $d
+    fi
+done

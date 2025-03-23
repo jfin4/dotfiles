b8311cdb723f51927e77326e36804ea3797f8554
John Inman <john@johnfinman.com>
2025-03-23 12:14:10 -0700

    no message

---
+++ set-up-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+[ -z "$1" ] && echo need branch && exit
+branch=$1
+
+git_dir=$HOME/.dotfiles.git
+[ -d $git_dir ] && \rm -rf $git_dir
+git clone --bare https://github.com/jfin4/dotfiles $git_dir
+
+function dot {
+   git --git-dir=$git_dir --work-tree=$HOME "$@"
+}
+dot switch $branch 2> /dev/null
+if [ $? != 0 ]; then
+    backup_dir=$HOME/dotfiles-backup
+    [ -d $backup_dir ] && \rm -rf $backup_dir
+    mkdir -p $backup_dir
+    dot switch $branch 2>&1 | grep -Pe '\t' | sed -e 's/\t\([^/]*\).*/\1/' | uniq \
+        | xargs -I{} mv {} $backup_dir/{}
+    dot switch $branch
+fi
